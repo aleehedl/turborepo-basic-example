e4976009dc2a9c9c4356941f3f356819e0e1b2fb
Alex Hedlund <alex.hedlund@vincit.fi>
2022-04-28 14:26:10 +0300

    Change button color

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button style={{ "backgroundColor": "#ff2233", "borderColor": "#ff0000" }} onClick={() => {alert('Beep');}}>Boop</button>;
+  return <button style={{ "backgroundColor": "#2222ff", "borderColor": "#0000ff" }} onClick={() => {alert('Beep');}}>Boop</button>;
 };
